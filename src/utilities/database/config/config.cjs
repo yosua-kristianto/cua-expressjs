e23400cdab2288d69af53c9815886cde015b2689
Yosua Kristianto <yosuakristianto754@gmail.com>
2022-07-27 08:56:36 +0700

    Added sequelize-cli--migration

---
+++ src/utilities/database/config/config.cjs
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+console.log(process.env)
+
+module.exports = {
+  "main": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOSTNAME,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_CONNECTION
+  }
+}
